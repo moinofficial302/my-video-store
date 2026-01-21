23efa50a1ff4ac79431b388e2026b63728d1116d
moinofficial302 <moinofficial302@gmail.com>
2026-01-21 22:46:59 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,11 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdgT-GBSctb1xZyW1s",
+  apiKey: "AIzaSyAdgT-GBSctb1xZwjVsVaH6ggBh1LYTAJQ",
   authDomain: "akans-d2bdc.firebaseapp.com",
-  projectId: "akans-d2bdc"
+  projectId: "akans-d2bdc",
+  storageBucket: "akans-d2bdc.appspot.com",
+  messagingSenderId: "588727557262",
+  appId: "1:588727557262:web:bdbe85977c97284837ddf"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
