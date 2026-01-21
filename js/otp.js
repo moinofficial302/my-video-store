2734f3bf10a891844ba6709eceee619730c46abf
moinofficial302 <moinofficial302@gmail.com>
2026-01-21 23:39:10 +0530

    Update otp.js

--- js/otp.js
+++ js/otp.js
@@ -1,23 +1,27 @@
 import { auth } from "./firebase.js";
-import { signInWithCredential, PhoneAuthProvider } 
-from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  PhoneAuthProvider,
+  signInWithCredential
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 document.getElementById("verifyOtpBtn").addEventListener("click", async () => {
   const otp = document.getElementById("otp").value;
+  const verificationId = localStorage.getItem("verificationId");
 
-  if (!otp) {
-    alert("OTP daalo");
+  if (!otp || !verificationId) {
+    alert("OTP ya session missing hai");
     return;
   }
 
   try {
     const credential = PhoneAuthProvider.credential(
-      window.confirmationResult.verificationId,
+      verificationId,
       otp
     );
 
     await signInWithCredential(auth, credential);
 
+    localStorage.removeItem("verificationId");
     window.location.href = "account.html";
   } catch (error) {
     alert("Invalid OTP");
