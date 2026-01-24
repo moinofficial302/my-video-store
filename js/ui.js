8731357aefccb9ef0cff6ea1fdf72e9b0211996c
moinofficial302 <moinofficial302@gmail.com>
2026-01-25 00:45:31 +0530

    Update ui.js not 🚫

--- js/ui.js
+++ js/ui.js
@@ -1,17 +1,1 @@
-function switchTab(tabId, el) {
-  // Hide all sections
-  document.querySelectorAll(".tab-section").forEach(sec => {
-    sec.classList.remove("active");
-  });
 
-  // Remove active from nav
-  document.querySelectorAll(".nav-item").forEach(btn => {
-    btn.classList.remove("active");
-  });
-
-  // Show selected section
-  document.getElementById(tabId).classList.add("active");
-
-  // Activate nav button
-  el.classList.add("active");
-}
